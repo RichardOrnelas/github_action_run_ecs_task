7c97054520ed8c185cabe2c5c64596f36881d0f8
Richard Ornelas <rich@terriblyrare.com>
2020-05-28 19:49:44 -0700

    keep pushing

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 # Container image that runs your code
 FROM ruby:2.6.5
+ENV LANG C.UTF-8
 
+RUN mkdir -p /var/app
+RUN mkdir -p /var/runs
+WORKDIR /var/app
+RUN gem update --system
+RUN bundle install
 # Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.rb /entrypoint.rb
+COPY entrypoint.rb /var/app/entrypoint.rb
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["ruby","/entrypoint.rb"]
---
+++ entrypoint.rb
@@ -0,0 +1,51 @@
+require "awesome_print"
+require "aws-sdk-ecs"
+require "shellwords"
+
+DESIRED_COUNT = 1
+TASK_DEFINITION = ARGV[0]
+CLUSTER = ARGV[1]
+COMMAND = ARGV[2]
+CONTAINER_NAME = ARGV[3]
+REGION = ARGV[4]
+SUBNET = ARGV[5]
+SECURITY_GROUP = ARGV[6]
+
+command = COMMAND.shellsplit
+
+ECS = Aws::ECS::Client.new()
+
+run_task = ECS.run_task({
+  cluster: CLUSTER,
+  task_definition: TASK_DEFINITION,
+  count: 1,
+  launch_type: "FARGATE",
+  network_configuration: {
+    awsvpc_configuration: {
+      subnets: [SUBNET],
+      security_groups: [SECURITY_GROUP]
+    }
+  }
+  overrides: {
+    container_overrides: [
+      name: CONTAINER_NAME,
+      command: command
+    ]
+  }
+})
+
+task_arn = run_task.tasks[0].task_arn
+
+ECS.wait_until(:tasks_stopped, { cluster: CLUSTER, tasks:[ task_arn ]}, { max_attempts: 10, delay: 10 }) do |w|
+  logger.info("Waiting for task to stop #{task_arn}")
+end
+logger.info("Waiting for task to stop #{task_arn}")
+
+task = ECS.describe_task({
+  cluster: CLUSTER,
+  tasks: [ task_arn ]
+})
+
+task_exit_code = task.tasks[0].containers[0].exit_code
+
+exit(task_exit_code || 255)
